0d416099cd4ab5084a1466b292ad3d19d58e7d36
IgorMushk <igor.mushketik@gmail.com>
2023-12-04 16:04:28 +0200

    done

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
 const app = require('./app')
+const dbConnect = require("./db/connection");
+const {PORT} = process.env;
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const startServer = async () =>{
+  try {
+    await dbConnect();
+    app.listen(PORT, () => {
+      console.log(`Server running. Use our API on port: ${PORT}`)
+    })
+  } catch (error) {
+    console.log(error); 
+  }
+}
+// app.listen(3000, () => {
+//   console.log("Server running. Use our API on port: 3000")
+// })
+startServer();
